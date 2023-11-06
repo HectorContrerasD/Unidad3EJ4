04c66f1c23e141e55eaaba12bacd1d06490054f1
HectorContrerasD <201G0239@rcarbonifera.tecnm.mx>
2023-11-06 16:26:39 -0600

    Repository

--- FruitStore/Program.cs
+++ FruitStore/Program.cs
@@ -1,10 +1,13 @@
 using FruitStore.Models.Entities;
+using FruitStore.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddTransient<Repository<Categorias>>();
+builder.Services.AddTransient<Repository<Productos>>();
+//CLase que es utilizada por muchas clases (servicio)
 builder.Services.AddMvc();
 builder.Services.AddDbContext<FruteriashopContext>(
     x=> x.UseMySql("user=root;password=root;server=localhost;database=fruteriashop", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql")));
--- FruitStore/Repositories/Repository.cs
+++ FruitStore/Repositories/Repository.cs
@@ -11,9 +11,31 @@ namespace FruitStore.Repositories
 
         public FruteriashopContext Context { get; }
 
-        //public virtual IEnumerable<T> GetAll()
-        //{
-        //    return 
-        //}
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        } 
+        public virtual T? Get(object Id)
+        {
+            return Context.Find<T>(Id);
+        }
+        public virtual void Insert(T entity) 
+        {
+             Context.Add(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Update(T entity)
+        {
+            Context.Update(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Delete(object Id) 
+        { 
+            var entity = Context.Find<T>(Id);
+            if (entity != null) 
+            {
+                Delete(entity);
+            }
+        }
     }
 }
