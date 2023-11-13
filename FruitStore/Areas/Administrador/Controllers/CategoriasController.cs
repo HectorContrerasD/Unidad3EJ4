5e05ab870b64cf47b57f6994de5d496d157adb45
HectorContrerasD <201G0239@rcarbonifera.tecnm.mx>
2023-11-13 15:48:41 -0600

    Agg categoria

--- FruitStore/Areas/Administrador/Controllers/CategoriasController.cs
+++ FruitStore/Areas/Administrador/Controllers/CategoriasController.cs
@@ -1,13 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FruitStore.Areas.Administrador.Models;
+using FruitStore.Models.Entities;
+using FruitStore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+using System.Collections.Immutable;
 
 namespace FruitStore.Areas.Administrador.Controllers
 {
     [Area("Administrador")]
     public class CategoriasController : Controller
     {
+        public CategoriasController(Repositories.Repository<Categorias> repository)
+        {
+             Repository = repository;
+        }
+
+        public Repository<Categorias> Repository { get; }
+
         public IActionResult Index()
         {
+            AdminCategoriaViewModel vm = new();
+            vm.Categorias = Repository.GetAll().OrderBy(x=>x.Nombre).Select(x=> new CategoriaModel
+            {
+                Id= x.Id,
+                Nombre = x.Nombre ??""
+            });
+            
+            return View(vm);
+        }
+
+        public IActionResult Agregar() 
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Categorias categorias)
+        {
+            if (string.IsNullOrWhiteSpace(categorias.Nombre)) 
+            {
+                ModelState.AddModelError("", "Escriba el nombre de la categoría");
+            }
+            if (ModelState.IsValid)
+            {
+                Repository.Insert(categorias);
+                return RedirectToAction("Index");
+            }
+            return View(categorias);
         }
     }
 }
---
+++ FruitStore/Areas/Administrador/Models/AdminCategoriaViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FruitStore.Areas.Administrador.Models
+{
+    public class AdminCategoriaViewModel
+    {
+        public IEnumerable<CategoriaModel> Categorias { get; set; } = null!;
+    }
+    public class CategoriaModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+}
