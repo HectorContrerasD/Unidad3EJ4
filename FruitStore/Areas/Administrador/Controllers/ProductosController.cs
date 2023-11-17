e7a67e0beec7f1a287cbbf9fa6f55a38428d6713
HectorContrerasD <201G0239@rcarbonifera.tecnm.mx>
2023-11-16 18:40:42 -0600

    productos

---
+++ FruitStore/Areas/Administrador/Controllers/ProductosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FruitStore.Areas.Administrador.Controllers
+{
+    public class ProductosController : Controller
+    {
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FruitStore/Areas/Administrador/Models/AdminProductosViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FruitStore.Areas.Administrador.Models
+{
+    public class AdminProductosViewModel
+    {
+        public int IdCategoriaSeleccionada { get; set; }
+        public IEnumerable<CategoriaModel> Categorias { get; set; } = null!;
+        public IEnumerable<ProductosModel> Productos { get; set; } = null!;
+
+    }
+    public class ProductosModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Categoria { get; set; } = null!;
+
+    }
+}
