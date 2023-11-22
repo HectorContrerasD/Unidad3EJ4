f44858f0692ab67d23c640524691f5221b7306a8
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2023-11-22 15:51:25 -0600

    Editar

--- FruitStore/Areas/Administrador/Controllers/ProductosController.cs
+++ FruitStore/Areas/Administrador/Controllers/ProductosController.cs
@@ -4,6 +4,7 @@ using FruitStore.Repositories;
 using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Expressions;
+using System.Security.Permissions;
 
 namespace FruitStore.Areas.Administrador.Controllers
 {
@@ -11,10 +12,10 @@ namespace FruitStore.Areas.Administrador.Controllers
     public class ProductosController : Controller
     {
         private readonly Repository<Categorias> categoriasRepository;
-        private readonly ProductosRepository productsRepository;
+        private readonly ProductosRepository productosRepository;
         public ProductosController(ProductosRepository productosRepository, Repository<Categorias> categoriasRepository)
         {   
-            this.productsRepository = productosRepository;
+            this.productosRepository = productosRepository;
             this.categoriasRepository = categoriasRepository;
                 
         }
@@ -29,7 +30,7 @@ namespace FruitStore.Areas.Administrador.Controllers
             });
             if (vm.IdCategoriaSeleccionada ==0)
             {
-                vm.Productos = productsRepository.GetAll().Select(x => new ProductosModel
+                vm.Productos = productosRepository.GetAll().Select(x => new ProductosModel
                 {
                     Id = x.Id,
                     Nombre = x.Nombre ?? "",
@@ -38,7 +39,7 @@ namespace FruitStore.Areas.Administrador.Controllers
             }
             else
             {
-                vm.Productos = productsRepository.GetProductosByCategorias(vm.IdCategoriaSeleccionada).Select(x => new ProductosModel
+                vm.Productos = productosRepository.GetProductosByCategorias(vm.IdCategoriaSeleccionada).Select(x => new ProductosModel
                 {
                     Id = x.Id,
                     Nombre = x.Nombre ?? "",
@@ -73,15 +74,79 @@ namespace FruitStore.Areas.Administrador.Controllers
                 }
                 if (ModelState.IsValid)
                 {
-                    productsRepository.Insert(vm.Producto);
+                    productosRepository.Insert(vm.Producto);
                     if (vm.Archivo == null)
                     {
-
+                        System.IO.File.Copy("wwwroot/img_frutas/0.jpg", $"wwwroot/img_frutas/{vm.Producto.Id}.jpg");
+                    }
+                    else
+                    {
+                        System.IO.FileStream fs=
+                        System.IO.File.Create($"wwwroot/img_frutas/{vm.Producto.Id}.jpg");
+                        vm.Archivo.CopyTo(fs);
                     }
 
                 }
+                return RedirectToAction("Index");
             }
-            return View();
+            return View(vm);
+        }
+        public IActionResult Editar(int id)
+        {
+            var producto = productosRepository.Get(id);
+            if (producto == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                AdminAgregarProductosViewModel vm = new AdminAgregarProductosViewModel();
+                vm.Producto = producto;
+                vm.Categorias = categoriasRepository.GetAll().OrderBy(x => x.Nombre)
+                .Select(x => new CategoriaModel
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre ?? ""
+                });
+                return View(vm);
+            }
+           
+        }
+        [HttpPost]
+        public IActionResult Editar(AdminAgregarProductosViewModel vm)
+        {
+
+            if (vm.Archivo != null)
+            {
+                if (vm.Archivo.ContentType != "image/jpeg")
+                {
+                    ModelState.AddModelError("", "Solo se permiten imagenes jpeg");
+                }
+                if (vm.Archivo.Length > 500 * 1024)
+                {
+                    ModelState.AddModelError("", "Solo se permiten archivos no mayores a 500Kb");
+                }
+                if (ModelState.IsValid)
+                {
+                    var producto = productosRepository.Get(vm.Producto.Id);
+                    if (producto == null)
+                    {
+                        return RedirectToAction("index");
+                    }
+                    else
+                    {
+                        producto.Nombre = vm.Producto.Nombre;
+                        producto.Precio = vm.Producto.Precio;
+                        producto.Descripcion = vm.Producto.Descripcion;
+                        producto.IdCategoria = vm.Producto.IdCategoria;
+                        producto.UnidadMedida = vm.Producto.UnidadMedida;
+                        productosRepository.Update(producto);
+                    }
+
+                }
+                return RedirectToAction("Index");
+            }
+            return View(vm);
         }
     }
 }
--- FruitStore/Areas/Administrador/Models/AdminAgregarProductosViewModel.cs
+++ FruitStore/Areas/Administrador/Models/AdminAgregarProductosViewModel.cs
@@ -4,7 +4,7 @@ namespace FruitStore.Areas.Administrador.Models
 {
     public class AdminAgregarProductosViewModel
     {
-        public Productos? Producto { get; set; } = null!;
+        public Productos Producto { get; set; } =new();
         public IEnumerable<CategoriaModel> Categorias { get; set; } = null!;
         public IFormFile Archivo { get; set; } = null!;
 
